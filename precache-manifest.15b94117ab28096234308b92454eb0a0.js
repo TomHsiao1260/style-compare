c42f9a9f325940b83eb41a26bc5f4553dbbb46bc
Hsiao Yao <tomtogo1260@gmail.com>
2020-08-17 02:44:22 +0800

    Updates

---
+++ precache-manifest.15b94117ab28096234308b92454eb0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc75a58848937fd12e4691da2dfd8367",
+    "url": "/style-compare/index.html"
+  },
+  {
+    "revision": "dcc66e0f5a9ec9190fc7",
+    "url": "/style-compare/static/css/main.c1a422da.chunk.css"
+  },
+  {
+    "revision": "05e1df23d75f3d36e9cf",
+    "url": "/style-compare/static/js/2.e7b0b393.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/style-compare/static/js/2.e7b0b393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc66e0f5a9ec9190fc7",
+    "url": "/style-compare/static/js/main.a2257d2a.chunk.js"
+  },
+  {
+    "revision": "6f127ed1436fe4cd0232",
+    "url": "/style-compare/static/js/runtime-main.6813fa06.js"
+  }
+]);
